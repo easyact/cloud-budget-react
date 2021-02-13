cb7f52368e933057db73d9082cb6069a8036dfd6
zhaolei <zhaolei@easyact.cn>
2021-02-13 16:29:52 +0800

    + getList service

--- src/components/budget/service/BudgetService.ts
+++ src/components/budget/service/BudgetService.ts
@@ -1,6 +1,6 @@
-import {BudgetRepository} from "../repository/BudgetRepository";
-import {ReaderTask} from "fp-ts/ReaderTask";
-import {Option} from "fp-ts/Option";
+import {BudgetRepository} from '../repository/BudgetRepository'
+import {ReaderTask} from 'fp-ts/ReaderTask'
+import {Option} from 'fp-ts/Option'
 
 export type BudgetOp<A> = ReaderTask<BudgetRepository, A>
 
@@ -8,6 +8,10 @@ export interface BudgetService {
     getAsset(user: string, version: string): BudgetOp<Option<any>>
 
     setAsset(user: string, version: string, v: any): BudgetOp<void>
+
+    getList(user: string, version: string, name: string): BudgetOp<Option<any>>
+
+    setList(user: string, version: string, name: string, v: any): BudgetOp<void>
 
     // useLiability(user: string, version: string): BudgetOp<[]>
 }
--- src/components/budget/service/interpreter/BudgetServiceInterpreter.ts
+++ src/components/budget/service/interpreter/BudgetServiceInterpreter.ts
@@ -1,18 +1,16 @@
-import {BudgetService} from "../BudgetService";
-import {asks} from "fp-ts/Reader";
-import {BudgetRepository} from "../../repository/BudgetRepository";
-import {Task} from "fp-ts/Task";
-import {Option} from "fp-ts/Option";
-import {pipe} from "fp-ts/function";
+import {BudgetOp, BudgetService} from '../BudgetService'
+import {asks} from 'fp-ts/Reader'
+import {BudgetRepository} from '../../repository/BudgetRepository'
+import {Task} from 'fp-ts/Task'
+import {Option} from 'fp-ts/Option'
 
 export class BudgetServiceInterpreter implements BudgetService {
-    getAsset = (user: string, version: string) =>
-        asks<BudgetRepository, Task<Option<any>>>(pipe(
-            repo => repo.getAssets(user, version),
-            // map(JSON.parse)
-        ));
-    setAsset = (user: string, version: string, v: any) =>
-        asks<BudgetRepository, Task<void>>(repo => repo.setAssets(user, version, v))
+    getAsset = (user: string, version: string) => asks<BudgetRepository, Task<Option<any>>>(repo => repo.getAssets(user, version))
+    setAsset = (user: string, version: string, v: any) => asks<BudgetRepository, Task<void>>(repo => repo.setAssets(user, version, v))
+
+    getList = (user: string, version: string, name: string): BudgetOp<Option<any>> => asks<BudgetRepository, Task<Option<any>>>(repo => repo.getList(user, version, name))
+
+    setList = (user: string, version: string, name: string, v: any): BudgetOp<void> => asks<BudgetRepository, Task<void>>(repo => repo.setList(user, version, name, v))
 
     // useLiability = (user: string, version: string): BudgetOp<[]> => undefined;
 
