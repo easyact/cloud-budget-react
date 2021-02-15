37e3a6f43b18ff4d0953cb653a2add3771e7acd2
zhaolei <zhaolei@easyact.cn>
2021-02-15 11:20:10 +0800

    + register

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,4 +1,3 @@
-import {Link} from 'react-router-dom'
 import {useEffect} from 'react'
 
 export function LandingPage() {
@@ -18,18 +17,18 @@ export function LandingPage() {
     }, []);
 
     return <section className="hero is-light is-bold">
-        <div className="hero-body">
-            <div className="container">
-                <h1 className="title">
-                    从996到财务自由
-                </h1>
-                <h2 className="subtitle">
-                    通过目标明确预算，马上开始行动.
-                </h2>
-                <p>无需繁复的记账, 只需每月比对估算与实际的偏差, 并调整预算即可.</p>
-                <p>设定财务目标, 确定差距, 选择最适合您的财务手段, 开始行动.</p>
-                <Link to="/budget" className="button is-large is-primary">开始预算</Link>
-            </div>
-        </div>
+        {/*<div className="hero-body">*/}
+        {/*    <div className="container">*/}
+        {/*        <h1 className="title">*/}
+        {/*            从996到财务自由*/}
+        {/*        </h1>*/}
+        {/*        <h2 className="subtitle">*/}
+        {/*            通过目标明确预算，马上开始行动.*/}
+        {/*        </h2>*/}
+        {/*        <p>无需繁复的记账, 只需每月比对估算与实际的偏差, 并调整预算即可.</p>*/}
+        {/*        <p>设定财务目标, 确定差距, 选择最适合您的财务手段, 开始行动.</p>*/}
+        {/*        <Link to="/budget" className="button is-large is-primary">开始预算</Link>*/}
+        {/*    </div>*/}
+        {/*</div>*/}
     </section>
 }
