fbbc352530952810b317f8463478f7ed74f74f69
zhaolei <zhaolei@easyact.cn>
2021-02-20 16:56:57 +0800

    * login

--- src/auth/Profile.js
+++ src/auth/Profile.js
@@ -2,14 +2,14 @@ import React from 'react'
 import {useAuth0} from '@auth0/auth0-react'
 import LogoutButton from './LogoutButton'
 import LoginButton from './LoginButton'
-import {Link} from 'react-router-dom'
 import {FaBug, FaSignOutAlt} from 'react-icons/all'
+import Loading from './Loading'
 
 const Profile = () => {
     const {user, isAuthenticated, isLoading} = useAuth0()
 
     if (isLoading) {
-        return <div>Loading ...</div>
+        return <div className="navbar-item"><Loading/></div>
     }
 
     return isAuthenticated ? <div className="navbar-item has-dropdown is-hoverable">
@@ -19,12 +19,10 @@ const Profile = () => {
         <div className="navbar-dropdown">
             <img src={user.picture} alt={user.name}/>
             <div>{user.email}</div>
-            <Link to="https://jinshuju.net/f/JXBXK2" className="navbar-item">
-                <div>
-                    <FaBug className="is-small"/>
-                    上报bug
-                </div>
-            </Link>
+            <a href="https://jinshuju.net/f/JXBXK2" className="navbar-item" target="_blank" rel="noreferrer">
+                <FaBug className="is-small"/>
+                上报bug
+            </a>
             <LogoutButton className="navbar-item">
                 <FaSignOutAlt className="is-small"/> 登出
             </LogoutButton>
