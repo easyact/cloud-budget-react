78957eb4b97e38eb476ce20b2ffc78f35a4cce4f
zhaolei <zhaolei@easyact.cn>
2021-04-14 11:36:48 +0800

    + monoid budget

--- src/components/budget/Model.ts
+++ src/components/budget/Model.ts
@@ -1,11 +1,39 @@
+import {Monoid} from 'fp-ts/Monoid'
+import {findIndex, map, modifyAt, reduce} from 'fp-ts/Array'
+import {pipe} from 'fp-ts/function'
+import * as O from 'fp-ts/Option'
+import R from 'ramda'
+
 export class Budget {
-
+    assets?: Item[] = []
+    liabilities?: Item[] = []
 }
 
-export class AssetList {
-
+const NULL = new Budget()
+export const BudgetAdditionMonoid: Monoid<Budget> = {
+    concat: (x, y) => pipe(NULL, R.keys,
+        map(k => R.objOf(k)(listAdditionMonoid.concat(x[k] ?? [], y[k] ?? []))),
+        R.mergeAll),
+    empty: new Budget()
 }
 
-export class LiabilityList {
+const defaultDate = new Date(0)
+const lastModifyAt = (item: Item) => item.lastModifiedDate ?? defaultDate
+// bindTo
+export const listAdditionMonoid: Monoid<Item[]> = {
+    concat: (x, y) => reduce(x, (r, item: Item) => pipe(
+        r,
+        findIndex(i => i.id === item.id),
+        O.chain(index => pipe(r, modifyAt(index, i => lastModifyAt(i) > lastModifyAt(item) ? i : item))),
+        // O.chain(f => f(r)),
+        O.getOrElse(() => r)
+    ))(y),
+    empty: []
+}
 
+export interface Item {
+    id?: String
+    name: String
+    amount: number
+    lastModifiedDate?: Date
 }
