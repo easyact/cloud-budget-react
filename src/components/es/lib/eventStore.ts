f92af136f442bbd993d1733b0337bdd7f3c272ec
zhaolei <zhaolei@easyact.cn>
2021-05-04 17:16:19 +0800

    * implements with error 'indexedDB API missing'

--- src/components/es/lib/eventStore.ts
+++ src/components/es/lib/eventStore.ts
@@ -1,4 +1,4 @@
-import {Event} from './es'
+import {Command, Event} from './es'
 import {Budget} from '../../budget/Model'
 import * as E from 'fp-ts/TaskEither'
 import {TaskEither} from 'fp-ts/TaskEither'
@@ -7,39 +7,51 @@ import Dexie from 'dexie'
 import {fromNullable} from 'fp-ts/lib/Either'
 
 export type Error = string
+type BEvent<T> = Event<T> & Command<T>
 
 export interface EventStore {
-    put(id: string, event: Event<Budget>): TaskEither<Error, void>
+    put(id: string, event: BEvent<Budget>): TaskEither<Error, any>
 
-    events(id: string): TaskEither<Error, Event<Budget>[]>
+    events(id: string): TaskEither<Error, BEvent<Budget>[]>
 }
 
 export class MemEventStore implements EventStore {
     private store = new Map()
 
-    put(id: string, event: Event<Budget>): TaskEither<Error, void> {
+    put(id: string, event: BEvent<Budget>): TaskEither<Error, any> {
         return pipe(
             this.events(id),
             E.map(es => [...es, event]),
             E.map(es => this.store.set(id, es)),
-            E.map(_ => undefined),
         )
     }
 
-    events = (id: string): TaskEither<Error, Event<Budget>[]> =>
+    events = (id: string): TaskEither<Error, BEvent<Budget>[]> =>
         E.fromEither(fromNullable('none')(this.store.get(id) ?? []))
 }
 
+class MyAppDatabase extends Dexie {
+    events: Dexie.Table<BEvent<Budget>, number>
+
+    constructor() {
+        super('easyact-budget')
+        this.version(1).stores({
+            events: '++id, user.email, at, version',
+            //...other tables goes here...
+        })
+        // The following line is needed if your typescript
+        // is compiled using babel instead of tsc:
+        this.events = this.table('events')
+    }
+}
+
 export class DBEventStore implements EventStore {
-    constructor(db: Dexie = new Dexie('easyact-budget')) {
-        db.version(1).stores({events: '++id, user.email, at, version'})
-    }
+    private db: MyAppDatabase = new MyAppDatabase()
 
-    events(id: string): TaskEither<Error, Event<Budget>[]> {
-        return E.left('later')
-    }
+    events = (id: string): TaskEither<Error, BEvent<Budget>[]> =>
+        E.fromTask(() => this.db.events.where('user.email').equals(id).toArray())
 
-    put(id: string, event: Event<Budget>): TaskEither<Error, void> {
-        return E.left('later')
+    put(id: string, event: BEvent<Budget>): TaskEither<Error, any> {
+        return E.fromTask(() => this.db.events.put(event))
     }
 }
