9cdb2d101cb02c8fde9c012b4a372e1d10a57825
zhaolei <zhaolei@easyact.cn>
2021-05-15 13:30:06 +0800

    - by static func

--- src/components/es/lib/es.ts
+++ src/components/es/lib/es.ts
@@ -37,17 +37,17 @@ export class BudgetSnapshot extends Snapshot<Budget> {
         const {user: {email}, to: {version}} = e
         const versions = initial.get(email) ?? new Map<string, Budget>()
         const budget = versions.get(version) ?? {}
-        const updatedBudget = this.updateBudget(e, budget)
+        const updatedBudget = BudgetSnapshot.updateBudget(e, budget)
         return initial.set(email, versions.set(version, updatedBudget))
     }
 
-    updateBudget(e: Event<Budget>, budget: Budget): Budget {
+    static updateBudget(e: Event<Budget>, budget: Budget): Budget {
         switch (e.type) {
             case 'IMPORT_BUDGET':
-                return this.importBudget(budget, e.payload)
+                return BudgetSnapshot.importBudget(budget, e.payload)
             case 'PUT_ITEM':
                 const {payload} = e
-                return this.importBudget(budget, {[payload.type]: [payload]})
+                return BudgetSnapshot.importBudget(budget, {[payload.type]: [payload]})
             case 'DELETE_ITEM':
                 const {payload: {from, id: deletingId}} = e
                 return R.over(R.lensProp(from), R.filter(({id}) => id !== deletingId))(budget)
@@ -56,7 +56,7 @@ export class BudgetSnapshot extends Snapshot<Budget> {
         }
     }
 
-    private importBudget = (budget: Budget, importing: Budget) => budgetAdditionMonoid.concat(budget, importing)
+    private static importBudget = (budget: Budget, importing: Budget) => budgetAdditionMonoid.concat(budget, importing)
 }
 
 
