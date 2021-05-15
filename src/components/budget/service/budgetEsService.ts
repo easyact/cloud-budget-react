512408bd439b0b7b3893fac00472fe0dac60e88b
zhaolei <zhaolei@easyact.cn>
2021-05-15 13:06:01 +0800

    - by static func

--- src/components/budget/service/budgetEsService.ts
+++ src/components/budget/service/budgetEsService.ts
@@ -2,11 +2,13 @@ import {Budget} from '../Model'
 import {BudgetSnapshot, Snapshot} from '../../es/lib/es'
 import * as E from 'fp-ts/lib/TaskEither'
 import {TaskEither} from 'fp-ts/lib/TaskEither'
+import * as RE from 'fp-ts/lib/ReaderTaskEither'
 import * as T from 'fp-ts/lib/Task'
 import {pipe} from 'fp-ts/lib/function'
 import {DBEventStore, Error, EventStore} from '../../es/lib/eventStore'
 import * as R from 'ramda'
 import {v4 as uuid} from 'uuid'
+import {ReaderTaskEither} from 'fp-ts/ReaderTaskEither'
 
 type CommandType = 'IMPORT_BUDGET' | 'PUT_ITEM' | 'DELETE_ITEM'
 export type Command = {
@@ -69,20 +71,32 @@ export class BudgetEsService {
         )()
     }
 
-    private handleCommand(command: Command): TaskEither<Error, void> {
-        const {type, payload, user: {email}} = command
-        switch (type) {
-            case 'IMPORT_BUDGET':
-                const idFilled = R.mapObjIndexed(R.map(({id = uuid(), ...vs}) => ({id, ...vs})))(payload)
-                return this.eventStore.put(email, {...command, payload: idFilled, at: new Date()})
-            case 'PUT_ITEM':
-                const lens = R.lensProp('id')
-                const item = R.over(lens, R.defaultTo(uuid()))(payload)
-                return this.eventStore.put(email, {...command, payload: item, at: new Date()})
-            case 'DELETE_ITEM':
-                return this.eventStore.put(email, {...command, at: new Date()})
-            default:
-                return E.left('不支持的命令')
-        }
+    private handleCommand = (command: Command): TaskEither<Error, void> => handleCommand(command)(this.eventStore)
+}
+
+function handleCommand(command: Command): ReaderTaskEither<EventStore, Error, void> {
+    const {type, payload, user: {email}} = command
+    switch (type) {
+        case 'IMPORT_BUDGET':
+            const idFilled = R.mapObjIndexed(R.map(({id = uuid(), ...vs}) => ({id, ...vs})))(payload)
+            return pipe(
+                RE.ask<EventStore>(),
+                RE.chainTaskEitherK(
+                    (s: EventStore) => s.put(email, {...command, payload: idFilled, at: new Date()})
+                ))
+        case 'PUT_ITEM':
+            const lens = R.lensProp('id')
+            const item = R.over(lens, R.defaultTo(uuid()))(payload)
+            return pipe(
+                RE.ask<EventStore>(),
+                RE.chainTaskEitherK((s: EventStore) => s.put(email, {...command, payload: item, at: new Date()})
+                ))
+        case 'DELETE_ITEM':
+            return pipe(
+                RE.ask<EventStore>(),
+                RE.chainTaskEitherK((s: EventStore) => s.put(email, {...command, at: new Date()})
+                ))
+        default:
+            return RE.left('不支持的命令')
     }
 }
