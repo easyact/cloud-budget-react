e9d081e6323362321e9e6ef4c05cc545d8cfc2a6
zhaolei <zhaolei@easyact.cn>
2021-05-18 13:43:59 +0800

    - 简化es

--- src/components/es/lib/es.ts
+++ src/components/es/lib/es.ts
@@ -2,10 +2,6 @@ import * as E from 'fp-ts/Either'
 import {Either} from 'fp-ts/Either'
 import {reduce} from 'fp-ts/Array'
 import {pipe} from 'fp-ts/function'
-import * as F from 'fp-ts-contrib/lib/Free'
-import * as T from 'fp-ts/lib/Task'
-import {Budget, budgetAdditionMonoid} from '../../budget/Model'
-import * as R from 'ramda'
 
 export type AggregateId = string
 
@@ -21,68 +17,8 @@ export interface Aggregate {
 
 export type BUDGET_SNAPSHOT<A> = Map<string, Map<string, A>>
 
-export abstract class Snapshot<A> {
-    abstract updateState(e: Event<A>, initial: BUDGET_SNAPSHOT<A>): BUDGET_SNAPSHOT<A>
-
-    snapshot = (es: Event<A>[], initial: BUDGET_SNAPSHOT<A> = new Map()): E.Either<string, BUDGET_SNAPSHOT<A>> => pipe(
-        es,
-        reduce(initial, (a, e) => this.updateState(e, a)),
-        E.right
-    )
-}
-
-export class BudgetSnapshot extends Snapshot<Budget> {
-    updateState(e: Event<Budget>, initial: BUDGET_SNAPSHOT<Budget>): BUDGET_SNAPSHOT<Budget> {
-        const {user: {email}, to: {version}} = e
-        const versions = initial.get(email) ?? new Map<string, Budget>()
-        const budget = versions.get(version) ?? {}
-        const updatedBudget = BudgetSnapshot.updateBudget(e, budget)
-        return initial.set(email, versions.set(version, updatedBudget))
-    }
-
-    static updateBudget(e: Event<Budget>, budget: Budget): Budget {
-        switch (e.type) {
-            case 'IMPORT_BUDGET':
-                return BudgetSnapshot.importBudget(budget, e.payload)
-            case 'PUT_ITEM':
-                const {payload} = e
-                return BudgetSnapshot.importBudget(budget, {[payload.type]: [payload]})
-            case 'DELETE_ITEM':
-                const {payload: {from, id: deletingId}} = e
-                return R.over(R.lensProp(from), R.filter(({id}) => id !== deletingId))(budget)
-            default:
-                return budget
-        }
-    }
-
-    private static importBudget = (budget: Budget, importing: Budget) => budgetAdditionMonoid.concat(budget, importing)
-}
-
 export const snapshot = <A>(updateState: (initial: BUDGET_SNAPSHOT<A>, e: Event<A>) => BUDGET_SNAPSHOT<A>, es: Event<A>[], initial: BUDGET_SNAPSHOT<A> = new Map(),): Either<string, BUDGET_SNAPSHOT<A>> => pipe(
     es,
     reduce(initial, updateState),
     E.right
 )
-
-
-export type Command<A> = F.Free<Event<A>, A>
-
-export interface Commands<A> {
-}
-
-export const URI = 'Event'
-export type URI = typeof URI
-declare module 'fp-ts/HKT' {
-    interface URItoKind<A> {
-        Event: Event<A>
-    }
-}
-
-export abstract class RepositoryBackedInterpreter<A> {
-    abstract step<A>(e: Event<A>): T.Task<A>
-
-    apply(action: F.Free<URI, A>): T.Task<any> {
-        return F.foldFree(T.task)(this.step, action)
-    }
-}
-
