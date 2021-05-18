759bf2a6c5c4cb3e3562236298be3329eba12096
zhaolei <zhaolei@easyact.cn>
2021-05-18 14:27:24 +0800

    + test thought

--- src/specs/register.spec.js
+++ src/specs/register.spec.js
@@ -1,12 +1,14 @@
 import path from 'path'
 import {Pact} from '@pact-foundation/pact'
 import {eachLike} from '@pact-foundation/pact/src/dsl/matchers'
+import {BudgetEsService} from '../components/budget/service/budgetEsService'
+import {MemEventStore} from '../components/es/lib/eventStore'
 
 const provider = new Pact({
     consumer: 'BudgetWebsite',
     provider: 'BudgetApiGateway',
     log: path.resolve(process.cwd(), 'logs', 'pact.log'),
-    logLevel: 'debug',
+    logLevel: 'warn',
     dir: path.resolve(process.cwd(), 'pacts'),
     spec: 2
 })
@@ -15,33 +17,31 @@ describe(`功能: 作为新用户, 为了注册后保留数据`, () => {
     beforeAll(() => provider.setup())
     afterEach(() => provider.verify())
     afterAll(() => provider.finalize())
+    const item = {name: 'TI', type: 'assets'}
 
     describe(`场景: 注册`, () => {
-        describe(`假设服务端没有damoco用户`, () => {
+        describe(`假设服务端没有damoco用户\n并且damoco在本地已有试用数据`, () => {
+            beforeEach(() => new BudgetEsService('default', new MemEventStore()).exec({type: 'IMPORT_BUDGET', payload: {}}))
             describe(`当damoco注册`, () => {
                 it(`那么简单将本地数据上传到服务器`, async () => {
-                    // set up Pact interactions
                     await provider.addInteraction({
-                        state: 'products exist',
-                        uponReceiving: 'get all products',
+                        state: 'no user',
+                        uponReceiving: 'first upload',
                         withRequest: {
-                            method: 'GET',
-                            path: '/products'
+                            method: 'POST',
+                            path: '/v0/users/damoco@easyact.cn/events',
+                            body: eachLike({
+                                id: '09',
+                                type: 'PUT_ITEM',
+                                at: '2021-05-14T00:00:00.012+08'
+                            })
                         },
                         willRespondWith: {
                             status: 200,
-                            headers: {
-                                'Content-Type': 'application/json; charset=utf-8'
-                            },
-                            body: eachLike({
-                                id: '09',
-                                type: 'CREDIT_CARD',
-                                name: 'Gem Visa'
-                            }),
                         },
                     })
 
-                    const api = new API(provider.mockService.baseUrl)
+                    const api = new RestApi(provider.mockService.baseUrl)
 
                     // make request to Pact mock server
                     const product = await api.getAllProducts()
