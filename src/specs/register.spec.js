4df7ce25ab446b5c5ccbc2d91b66ca00d740fe0a
zhaolei <zhaolei@easyact.cn>
2021-05-19 12:10:04 +0800

    * design

--- src/specs/register.spec.js
+++ src/specs/register.spec.js
@@ -1,8 +1,6 @@
 import path from 'path'
 import {Pact} from '@pact-foundation/pact'
 import {eachLike} from '@pact-foundation/pact/src/dsl/matchers'
-import {BudgetEsService} from '../components/budget/service/budgetEsService'
-import {MemEventStore} from '../components/es/lib/eventStore'
 
 const provider = new Pact({
     consumer: 'BudgetWebsite',
@@ -12,16 +10,22 @@ const provider = new Pact({
     dir: path.resolve(process.cwd(), 'pacts'),
     spec: 2
 })
+
+async function postEvents(baseUrl, events) {
+
+}
+
 describe(`功能: 作为新用户, 为了注册后保留数据`, () => {
 
     beforeAll(() => provider.setup())
     afterEach(() => provider.verify())
     afterAll(() => provider.finalize())
-    const item = {name: 'TI', type: 'assets'}
+    const item = {name: 'food', amount: 10}
 
     describe(`场景: 注册`, () => {
         describe(`假设服务端没有damoco用户\n并且damoco在本地已有试用数据`, () => {
-            beforeEach(() => new BudgetEsService('default', new MemEventStore()).exec({type: 'IMPORT_BUDGET', payload: {}}))
+            // const eventStore = new MemEventStore()
+            // beforeEach(importBudget('damoco', {assets: [item]})(eventStore))
             describe(`当damoco注册`, () => {
                 it(`那么简单将本地数据上传到服务器`, async () => {
                     await provider.addInteraction({
@@ -41,13 +45,11 @@ describe(`功能: 作为新用户, 为了注册后保留数据`, () => {
                         },
                     })
 
-                    const api = new RestApi(provider.mockService.baseUrl)
-
-                    // make request to Pact mock server
-                    const product = await api.getAllProducts()
-                    expect(product).toStrictEqual([
-                        {'id': '09', 'name': 'Gem Visa', 'type': 'CREDIT_CARD'}
-                    ])
+                    const resp = await postEvents(provider.mockService.baseUrl, [{
+                        type: 'IMPORT_BUDGET',
+                        payload: {assets: [item]}
+                    }])
+                    expect(resp).toStrictEqual(undefined)
                 })
             })
         })
