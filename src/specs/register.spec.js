2a29278a8472ad069b44db3727c4b039ffadefd8
zhaolei <zhaolei@easyact.cn>
2021-05-20 10:34:16 +0800

    + get events

--- src/specs/register.spec.js
+++ src/specs/register.spec.js
@@ -25,30 +25,30 @@ describe(`功能: 作为新用户, 为了注册后保留数据`, () => {
     describe(`场景: 注册`, () => {
         describe(`假设服务端没有damoco用户\n并且damoco在本地已有试用数据`, () => {
             const eventStore = new MemEventStore()
+            beforeEach(() => provider.addInteraction({
+                state: 'no user',
+                uponReceiving: 'first upload',
+                withRequest: {
+                    method: 'POST',
+                    path: '/v0/users/damoco@easyact.cn/events',
+                    body: eachLike({
+                        type: 'PUT_ITEM',
+                        at: '2021-05-14T00:00:00.012+08',
+                        user: {email: 'damoco'},
+                        to: {version: '0'},
+                        payload: {assets: [{id: '', ...item}]},
+                    }),
+                    // headers: {Accept: 'application/json'},
+                },
+                willRespondWith: {
+                    status: 200,
+                    headers: {'access-control-allow-origin': '*'},
+                    // body: ''
+                },
+            }))
             beforeEach(importBudget('damoco', {assets: [item]})(eventStore))
             describe(`当damoco注册`, () => {
-                it(`那么简单将本地数据上传到服务器`, async () => {
-                    await provider.addInteraction({
-                        state: 'no user',
-                        uponReceiving: 'first upload',
-                        withRequest: {
-                            method: 'POST',
-                            path: '/v0/users/damoco@easyact.cn/events',
-                            body: eachLike({
-                                type: 'PUT_ITEM',
-                                at: '2021-05-14T00:00:00.012+08',
-                                user: {email: 'damoco'},
-                                to: {version: '0'},
-                                payload: {assets: [{id: '', ...item}]},
-                            }),
-                            // headers: {Accept: 'application/json'},
-                        },
-                        willRespondWith: {
-                            status: 200,
-                            headers: {'access-control-allow-origin': '*'},
-                            // body: ''
-                        },
-                    })
+                beforeEach(async () => {
                     const url = provider.mockService.baseUrl + '/v0/users/damoco@easyact.cn/events'
                     expect(url).toBeTruthy()
 
@@ -65,38 +65,8 @@ describe(`功能: 作为新用户, 为了注册后保留数据`, () => {
                     })
                     expect(resp.ok).toStrictEqual(true)
                 })
-                // it(`那么简单将本地数据上传到服务器`, pipe(
-                //     eventStore.events('damoco'),
-                //     TE.fold(T.of, events => async () => {
-                //         expect(events).not.toHaveLength(0)
-                //         await provider.addInteraction({
-                //             state: 'no user',
-                //             uponReceiving: 'first upload',
-                //             withRequest: {
-                //                 method: 'POST',
-                //                 path: '/v0/users/damoco@easyact.cn/events',
-                //                 body: eachLike({
-                //                     id: '09',
-                //                     type: 'PUT_ITEM',
-                //                     at: '2021-05-14T00:00:00.012+08'
-                //                 })
-                //             },
-                //             willRespondWith: {
-                //                 status: 200,
-                //             },
-                //         })
-                //         const url = provider.mockService.baseUrl + '/v0/users/damoco@easyact.cn/events'
-                //         expect(url).toBeTruthy()
-                //         const resp = await fetch(url, {
-                //             method: 'POST',
-                //             headers: {
-                //                 'content-type': 'application/json'
-                //             },
-                //             body: JSON.stringify(events)
-                //         })
-                //         expect(resp).toStrictEqual(undefined)
-                //     })
-                // ))
+                it(`那么从新客户端访问可以拿到所有历史事件`, async () => {
+                })
             })
         })
     })
