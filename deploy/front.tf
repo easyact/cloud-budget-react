61d6fe7e351cdb9fc9f2fadb7b06785b999c957c
zhaolei <zhaolei@easyact.cn>
2021-05-22 07:53:07 +0800

    + same code deploy global success but cn not

---
+++ deploy/front.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "cn-northwest-1"
+}
+resource "aws_s3_bucket" "www" {
+  bucket = "easyact.cn"
+  acl = "public-read"
+  policy = file("policy.json")
+  website {
+    index_document = "index.html"
+  }
+}
+
+locals {
+  dist = "../build"
+  files = fileset(local.dist, "**")
+  mime_types = jsondecode(file("${path.module}/data/mime.json"))
+}
+
+resource "aws_s3_bucket_object" "files" {
+  for_each = local.files
+  bucket = aws_s3_bucket.www.bucket
+  key = each.key
+  source = "${local.dist}/${each.key}"
+  content_type = lookup(local.mime_types, regex("\\.[^.]+$", each.key), "binary/octet-stream")
+  //  acl =
+}
+
+output "www" {
+  value = {
+    s3 = aws_s3_bucket.www
+  }
+}
