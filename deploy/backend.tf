333bf88285db2034e9ea974e0bd72c762da91442
zhaolei <zhaolei@easyact.cn>
2021-05-25 11:37:06 +0800

    + api gateway mapping

--- deploy/backend.tf
+++ deploy/backend.tf
@@ -26,14 +26,29 @@ data "aws_iam_policy_document" "apig_sqs" {
   }
 }
 
-//resource "aws_iam_role_policy_attachment" "apig_sqs" {
-//  policy_arn = aws_iam_policy.apig_sqs_policy.arn
-//  role = aws_iam_role.apig_sqs.arn
-//}
+data "aws_iam_policy_document" "apig_dbd" {
+  statement {
+    actions = [
+      "dynamodb:PutItem"]
+    resources = ["arn:aws-cn:dynamodb:cn-northwest-1:020064664914:table/events"]
+  }
+}
+
+resource "aws_iam_policy" "apig_dbd" {
+  name = "apig_dbd_policy"
+  policy = data.aws_iam_policy_document.apig_dbd.json
+}
+
+resource "aws_iam_role" "apig_dbd" {
+  name = "apig_dbd"
+  assume_role_policy = data.aws_iam_policy_document.apig_sqs.json
+  managed_policy_arns = [
+    aws_iam_policy.apig_dbd.arn]
+}
 
 output "api" {
   value = {
     sqs = aws_sqs_queue.es
-    role = aws_iam_role.apig_sqs.arn
+    role = [aws_iam_role.apig_sqs.arn, aws_iam_role.apig_dbd.arn]
   }
 }
