9eebb600a14bd9c10b8d09a7dec3d97a68665f65
zhaolei <zhaolei@easyact.cn>
2021-05-30 21:08:55 +0800

    * offset

--- src/components/budget/service/budgetEsService.ts
+++ src/components/budget/service/budgetEsService.ts
@@ -3,6 +3,7 @@ import * as TE from 'fp-ts/lib/TaskEither'
 import * as RTE from 'fp-ts/lib/ReaderTaskEither'
 import * as RT from 'fp-ts/lib/ReaderTask'
 import * as T from 'fp-ts/lib/Task'
+import * as O from 'fp-ts/lib/Option'
 import {pipe} from 'fp-ts/lib/function'
 import {BEvent, DBEventStore, Error, EventStore} from '../../es/lib/eventStore'
 import * as R from 'ramda'
@@ -10,9 +11,14 @@ import {v4 as uuid} from 'uuid'
 import {ReaderTaskEither} from 'fp-ts/ReaderTaskEither'
 import {budgetSnapshot} from './snapshot'
 import {ReaderTask} from 'fp-ts/ReaderTask'
+import {getItem, setItem} from 'fp-ts-local-storage'
+import {last} from 'fp-ts/lib/Array'
+import {Lens} from 'monocle-ts'
+import {IO} from 'fp-ts/lib/IO'
 
 type CommandType = 'IMPORT_BUDGET' | 'PUT_ITEM' | 'DELETE_ITEM'
 export type Command = {
+    id?: number,
     type: CommandType, payload: any, user: { email: string }, to: {
         list?: string
         version: string
@@ -110,9 +116,26 @@ export const post = (url: string, obj: any) => fetch(url, {
 
 export const uploadEvents =
     (email: string, url: string): ReaderTaskEither<EventStore, string, { events: BEvent[], resp: Response }> => pipe(
-        getEvents(email), RTE.bindTo('events'),
-        RTE.bind('resp', ({events}) => RTE.fromTask(() => post(url, events)))
+        getEvents(email),
+        RTE.map(events => pipe(
+            eventOffset(email)(),
+            O.chain((s: string) => O.tryCatch(() => parseInt(s))),
+            O.getOrElse(() => 0),
+            offset => O.tryCatch(() => events.slice(offset)),
+            O.getOrElse(() => [] as BEvent[])
+        )),
+        RTE.bindTo('events'),
+        RTE.bind('resp', ({events}) => RTE.fromTask(() => post(url, events))),
+        RTE.chainFirst(({events}) => setOffsetByEvents(email, events)),
     )
-// export function register() {
-//
-// }
+
+export const eventOffset = (email: string) => getItem(`${email}.offset`)
+export const setEventOffset = (email: string) => (offset: number): IO<void> => setItem(`${email}.offset`, String(offset))
+const id: Lens<BEvent, BEvent[string]> = Lens.fromProp<BEvent>()('id')
+const setOffsetByEvents = (email: string, events: BEvent[]): ReaderTaskEither<EventStore, string, void> => pipe(
+    last(events),
+    O.map(id.get),
+    O.map(setEventOffset(email)),
+    RTE.fromOption(() => 'setOffsetByEvents got null id'),
+    RTE.map(f => f()),
+)
