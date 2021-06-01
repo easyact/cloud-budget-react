d45c83b3bf288876d715891c917c136bbf82d7c6
zhaolei <zhaolei@easyact.cn>
2021-06-01 17:36:49 +0800

    * Option for login strategy

--- src/components/budget/hook/useBudget.ts
+++ src/components/budget/hook/useBudget.ts
@@ -7,28 +7,49 @@ import * as O from 'fp-ts/Option'
 import {Option} from 'fp-ts/Option'
 import * as E from 'fp-ts/Either'
 import * as RTE from 'fp-ts/ReaderTaskEither'
-import * as io from 'fp-ts/IO'
+import {ReaderTaskEither} from 'fp-ts/ReaderTaskEither'
 import {IO} from 'fp-ts/IO'
 import log from '../../log'
-import {DBEventStore} from '../../es/lib/eventStore'
+import {DBEventStore, EventStore} from '../../es/lib/eventStore'
 import * as TE from 'fp-ts/TaskEither'
 import * as T from 'fp-ts/Task'
-import {getItem} from 'fp-ts-local-storage'
+import {getItem, setItem} from 'fp-ts-local-storage'
 import {pipe} from 'fp-ts/lib/function'
 
 const uidKey = 'user.id'
 const getUser = (): IO<Option<string>> => getItem(uidKey)
+const setUser = (user: string): IO<void> => setItem(uidKey, user)
 
 // function notRegistered() {
 //     return O.isNone(getUser()())
 // }
 
+function getA(email: string, localUser: Option<string>): ReaderTaskEither<EventStore, string, any> {
+    return O.fold(
+        () => migrateEventsToUser(email),
+        RTE.of
+    )(localUser)
+}
+
 export default function useBudget(version: string): [BudgetState, Dispatch<ReducerAction<any>>] {
-    const uid: string = pipe(
-        getUser(),
-        io.map(O.getOrElse(() => 'default')),
-        // io.chainFirst(s => setItem(uidKey, s))
-    )()
+    const {user: {email}, isAuthenticated} = useAuth0()
+    const localUser: Option<string> = getUser()()
+    const uid = pipe(
+        O.fromNullable(email),
+        O.fold(
+            () => O.fold(
+                () => RTE.of<EventStore, string, string>('default'),
+                (u: string) => RTE.of(u)
+            )(localUser),
+            () => pipe(
+                O.fold(
+                    () => migrateEventsToUser(email),
+                    RTE.of
+                )(localUser),
+                RTE.chainFirst(() => RTE.fromIO(setUser(email)))
+            )
+        )
+    )
     const [state, dispatch] = useReducer<Reducer<any, any>>(reducer, {
         uid,
         version,
@@ -39,16 +60,18 @@ export default function useBudget(version: string): [BudgetState, Dispatch<Reduc
         eventStore: new DBEventStore(),
         apiUrl: `https://grac2ocq56.execute-api.cn-northwest-1.amazonaws.com.cn/`
     })
-    const {isAuthenticated} = useAuth0()
     const {eventStore, cmd, apiUrl}: BudgetState = state
     console.log('useBudgeting', uid, version, state, eventStore)
     useEffect(function login() {
         if (!isAuthenticated) return
         dispatch({type: 'LOGGED_IN', uid})
         const readerTaskEither = pipe(
-            getUser(),
+            localUser,
             RTE.fromIO,
-            RTE.chain(O.fold(() => migrateEventsToUser(uid), RTE.of)),
+            RTE.chain(O.fold(() => {
+                setItem(uidKey, uid)()
+                return migrateEventsToUser(uid)
+            }, RTE.of)),
             RTE.chain(() => sync(uid, apiUrl)),
         )
         readerTaskEither(eventStore)().then(E.fold(
