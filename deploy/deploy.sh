4e48261b442e32f2740853a2898748971b493726
zhaolei <zhaolei@easyact.cn>
2021-06-21 16:35:58 +0800

    + GA

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,3 +4,5 @@ pushd $SHELL_FOLDER/..
 npm run build
 popd
 aws s3 sync $SHELL_FOLDER/../build s3://easyact.cn
+aws cloudfront create-invalidation --distribution-id $cfid \
+              --paths "/*"
