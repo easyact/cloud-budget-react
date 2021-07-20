d7599d2cea9fa13c6b2b5ab7ebf1618de42f8f7e
zhaolei <zhaolei@easyact.cn>
2021-07-20 12:18:45 +0800

    + breadcrumbs

--- src/components/price/Pay.js
+++ src/components/price/Pay.js
@@ -1,5 +1,13 @@
 import {Developing} from '../Developing'
 
 export function Pay() {
-    return <Developing/>
+    return <div>
+        <nav className="breadcrumb" aria-label="breadcrumbs">
+            <ul>
+                <li><a href="/">首页</a></li>
+                <li className="is-active"><a href="." aria-current="page">支付</a></li>
+            </ul>
+        </nav>
+        <Developing/>
+    </div>
 }
