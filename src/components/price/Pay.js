e38397ebfbec5a6713bd6a1c774010a9723fcafc
zhaolei <zhaolei@easyact.cn>
2021-07-30 15:04:11 +0800

    + js.hsforms.net/forms/shell.js

--- src/components/price/Pay.js
+++ src/components/price/Pay.js
@@ -1,6 +1,16 @@
-import {Developing} from '../Developing'
+import {useEffect} from 'react'
+import useScript from '../../hooks/useScript'
 
 export function Pay() {
+    useScript('//js.hsforms.net/forms/shell.js')
+    useEffect(() => {
+        window.onload = () =>
+            window.hbspt.forms.create({
+                region: 'na1',
+                portalId: '20302760',
+                formId: '70c48266-8015-4fbd-92c5-d627c78784c2'
+            })
+    }, [])
     return <div>
         <nav className="breadcrumb" aria-label="breadcrumbs">
             <ul>
@@ -8,6 +18,6 @@ export function Pay() {
                 <li className="is-active"><a href="." aria-current="page">支付</a></li>
             </ul>
         </nav>
-        <Developing/>
+        {/*<Developing/>*/}
     </div>
 }
--- src/specs/server/pact-verification.spec.js
+++ src/specs/server/pact-verification.spec.js
@@ -42,5 +42,5 @@ describe('Pact Verification', () => {
 
     it('should validate the expectations of Order Web', () => {
         return new Verifier(opts).verifyProvider()
-    }, 60000)
+    }, 80000)
 })
