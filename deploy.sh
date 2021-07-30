a88fc582ed90949156d8dc4215bb7bc2d0437803
zhaolei <zhaolei@easyact.cn>
2021-07-30 16:15:31 +0800

    * CI deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 set -x
 SHELL_FOLDER=$(dirname "$0")
+REGION=' --region cn-northwest-1 '
 npm run build
-aws s3 sync $SHELL_FOLDER/build s3://easyact.cn
+aws s3 sync $SHELL_FOLDER/build $REGION s3://easyact.cn
 aws cloudfront create-invalidation --distribution-id $cfid \
-              --paths "/*"
+              $REGION --paths "/*"
