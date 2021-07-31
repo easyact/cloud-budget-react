3dc99a0742b63fbfe7469679e1a7a40459bdccc4
zhaolei <zhaolei@easyact.cn>
2021-07-31 09:02:31 +0800

    + CI aws log

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 set -x
 SHELL_FOLDER=$(dirname "$0")
 npm run build
-aws s3 sync $SHELL_FOLDER/build s3://easyact.cn
-aws cloudfront create-invalidation --distribution-id $cfid \
+aws s3 sync --debug $SHELL_FOLDER/build s3://easyact.cn
+aws cloudfront create-invalidation --debug --distribution-id $cfid \
               --paths "/*"
