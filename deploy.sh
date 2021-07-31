2ca5ed78c5a677df8bef7241cd91dc698b1ee1d7
zhaolei <zhaolei@easyact.cn>
2021-07-31 08:27:09 +0800

    * CI deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 set -x
 SHELL_FOLDER=$(dirname "$0")
-echo $AWS_DEFAULT_REGION $AWS_ACCESS_KEY_ID	$AWS_SECRET_ACCESS_KEY
 npm run build
 aws s3 sync $SHELL_FOLDER/build s3://easyact.cn
 aws cloudfront create-invalidation --distribution-id $cfid \
