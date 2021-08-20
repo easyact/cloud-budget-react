c8a0de7681034f09bff8ced5c805d5475f3dace2
zhaolei <zhaolei@easyact.cn>
2021-08-20 15:17:34 +0800

    + 主页文案: 没有出头之日?

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -8,16 +8,25 @@ export function LandingPage() {
             <div className="hero-body">
                 <div className="container is-full-desktop">
                     <h1 className="title">
-                        从996到财务自由
+                        {/*从996到财务自由*/}
+                        没有出头之日?
                     </h1>
                     <h2 className="subtitle">
-                        利用预算的力量, 防止财务暴雷.
+                        利用预算的力量, 开始投资你的未来.
                         {/*利用预算的力量, 提升理财效率.*/}
                     </h2>
                     {/*<p>无需记账, 只需每月比对估算与实际的偏差, 调整计划.</p>*/}
-                    <p>利用可视化工具模拟未来的分期情况, 提前做好分期计划. </p>
                     {/*<p>选择适合的投资, 开始行动.</p>*/}
-                    <Link to="/budget" className="button is-large is-primary">免费试用</Link>
+                    {/*<p>利用可视化工具模拟未来的分期情况, 提前做好分期计划. </p>*/}
+                    {/*<p>利用可视化工具模拟未来的分期情况, 提前做好分期计划. </p>*/}
+                    <div className="field is-grouped is-grouped-centered">
+                        <p className="control">
+                            <Link to="/budget" className="button is-large is-primary">免费试用</Link>
+                        </p>
+                        <p className="control">
+                            <Link to="/budget" className="button is-large is-ghost">了解更多</Link>
+                        </p>
+                    </div>
                 </div>
             </div>
         </section>
