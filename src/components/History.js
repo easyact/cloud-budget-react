27ad8f6b23b98b704250d8181a1bb38f04481315
zhaolei <zhaolei@easyact.cn>
2021-09-04 18:18:40 +0800

    * 历史倒序

--- src/components/History.js
+++ src/components/History.js
@@ -1,9 +1,10 @@
 import {Trans} from 'react-i18next'
+import * as R from 'ramda'
 
 export function History({dispatch, events}) {
     const setLast = id => dispatch({type: 'SET_LAST_EVENT_ID', payload: id})
     return <div className="dropdown-content history">
-        {events.reverse().map((e, i) => <div className="dropdown-item" key={i}>
+        {R.reverse(events).map((e, i) => <div className="dropdown-item" key={i}>
             {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
             <a onClick={() => setLast(e.id)}><Trans>{e.type}</Trans> 版本: {e.to?.version}
                 <div className="ea-content">{JSON.stringify(e.payload)}</div>
