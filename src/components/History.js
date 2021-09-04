83c467735e58ff8f4c762addd36e005467f81d10
zhaolei <zhaolei@easyact.cn>
2021-09-04 16:52:17 +0800

    * 翻译历史

--- src/components/History.js
+++ src/components/History.js
@@ -1,9 +1,11 @@
+import {Trans} from 'react-i18next'
+
 export function History({dispatch, events}) {
     const setLast = id => dispatch({type: 'SET_LAST_EVENT_ID', payload: id})
     return <div className="dropdown-content history">
         {events.map((e, i) => <div className="dropdown-item" key={i}>
             {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-            <a onClick={() => setLast(e.id)}>{e.type} 分支: {e.to?.version}
+            <a onClick={() => setLast(e.id)}><Trans>{e.type}</Trans> 分支: {e.to?.version}
                 <div className="ea-content">{JSON.stringify(e.payload)}</div>
             </a>
         </div>)}
